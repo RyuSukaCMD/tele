e710fb2287aa834c4b7b8eeadea320e6028b9f66
RyuSukaCMD <regyseptiawan47@gmail.com>
2025-02-16 17:59:27 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,28 @@
+/*SUBSCRIBE MY YOUTUBE IRULZXD YT*/
+const fs = require("fs");
+const {
+   indonesia
+} = require("./language");
+
+// Website Api (jgn di ganti biar gk eror)
+global.APIs = {
+   alfa: 'https://api.zeeoneofc.my.id', //apabila link api eror, segera laporkan ke owner
+}
+
+//buy apikey premium 6281330251835
+// Free apikey (silahkan login terus ganti Your Key dgn apikey lu)
+global.APIKeys = {
+   'https://api.zeeoneofc.my.id': '6T8nj4b9zYHBlyL', // 👉 free apikey from irulz limit 30 !! 
+}
+
+//language 
+global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
+
+global.BOT_TOKEN = "7735183759:AAEgAb-mNbsyADyI6UaiTeedwL_j0kpvrdg" // buat bot di sini https://t.me/Botfather dan dapatkan token bot
+global.BOT_NAME = "Ryuu BOTZ" //your bot name
+global.OWNER_NAME = "RyuuItsMeeོ" //your name
+global.OWNER_NUMBER = "Kepo yeh" //your telegram number
+global.OWNER = ["https://t.me/RyuuStoreeOFFC", "https://t.me/RyuuStoreeOFFC"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
+global.THUMBNAIL = "./image/irulzthumb.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
+global.DONASI = "./image/donasi.jpg" // foto donasi di folder image
+global.lang = language //don't change
